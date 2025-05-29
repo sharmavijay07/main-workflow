8d92dfb4c61fe1ae35cedcfbe11ffb056633087e
sharmavijay07 <156803752+sharmavijay07@users.noreply.github.com>
2025-05-29 16:22:28 +0530

    Update notifications.js

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -202,7 +202,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASSWORD,
   },
 })
 
