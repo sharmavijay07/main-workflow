830111b497af3f40a4af8549b23acd1da7f056ed
sharmavijay07 <156803752+sharmavijay07@users.noreply.github.com>
2025-05-29 16:11:17 +0530

    Update usePushNotifications.js

--- client/src/hooks/usePushNotifications.js
+++ client/src/hooks/usePushNotifications.js
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import { initializePushNotifications, unsubscribeFromPushNotifications } from "../utlils/pushNotifications"
+import { initializePushNotifications, unsubscribeFromPushNotifications } from "../utils/pushNotifications"
 import { useAuth } from "../context/auth-context"
 
 export function usePushNotifications() {
