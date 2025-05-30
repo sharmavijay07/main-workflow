08391866bc9e3a279ea10c29f46d14a43e14d9cd
sharmavijay07 <156803752+sharmavijay07@users.noreply.github.com>
2025-05-30 12:55:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -161,7 +161,7 @@ const io = socketIo(server, {
 
 // CORS Configuration
 const corsOptions = {
-  origin: ['http://localhost:5173','http://192.168.1.2:5173','https://main-workflow.vercel.app'],  // Replace with your frontend's URL
+  origin: ['http://localhost:5173','http://192.168.1.2:5173','https://main-workflow.vercel.app','https://workflowmanager.vercel.app'],  // Replace with your frontend's URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,  // Allow credentials (cookies, HTTP authentication)
 };
