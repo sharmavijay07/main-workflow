8edd652dfcd74ba9ddb97677181b629a5af9d705
sharmavijay07 <156803752+sharmavijay07@users.noreply.github.com>
2025-05-30 12:51:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -153,7 +153,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: ['http://localhost:5173','http://192.168.1.2:5173','https://main-workflow.vercel.app'],  // Replace with your frontend's URL
+    origin: ['http://localhost:5173','http://192.168.1.2:5173','https://main-workflow.vercel.app','https://workflowmanager.vercel.app'],  // Replace with your frontend's URL
     methods: ['GET', 'POST'],
     credentials: true,  // Allow credentials (cookies, HTTP authentication)
   },
